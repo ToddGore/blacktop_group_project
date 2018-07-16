1434caa7be44faf124d3600f0439eeac30de633e
Blake Rindlisbacher <blakebacher@gmail.com>
2018-07-16 16:28:54 -0600

    made nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Nav.css'
+
+import Search from './../Images/search_icon.png'
+import Message from './../Images/messaging_icon.svg'
+
+export default function Nav (){
+     return(
+        <div>
+            <div className = 'Nav'>
+            <button className=''>
+                <img src = {Search} alt = ''/>
+            </button>
+            <button className=''>
+                <img src = {Message} alt = ''/>
+            </button>
+            <button className=''>
+                {/* <img src = {} alt = ''/> */}
+            </button>
+            <button className=''>
+                {/* <img src = {} alt = ''/> */}
+            </button>
+            </div> 
+        </div>
+     )
+}
