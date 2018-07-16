a4520b3a60ee00bea6faf5a8a142edc65610c254
Blake Rindlisbacher <blakebacher@gmail.com>
2018-07-16 17:29:42 -0600

    added components

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import './Nav.css'
+import {Link} from 'react-router-dom'
 
-import Search from './../Images/search_icon.png'
-import Message from './../Images/messaging_icon.svg'
+import Search_icon from './../Images/Search_icon.svg'
+import Message_icon from './../Images/Message_icon.svg'
+import Schedule_icon from './../Images/Schedule_icon.svg'
+import Menu_icon from './../Images/Menu_icon.svg'
 
 export default function Nav (){
      return(
         <div>
             <div className = 'Nav'>
-            <button className=''>
-                <img src = {Search} alt = ''/>
-            </button>
-            <button className=''>
-                <img src = {Message} alt = ''/>
-            </button>
-            <button className=''>
-                {/* <img src = {} alt = ''/> */}
-            </button>
-            <button className=''>
-                {/* <img src = {} alt = ''/> */}
-            </button>
+            <Link><li href = ''><img src = {Search_icon} alt = '' className = 'Size'/></li></Link>
+            <Link><li href = ''><img src = {Message_icon} alt = '' className = 'Size'/></li></Link>
+            <Link><li href = ''><img src = {Schedule_icon} alt = '' className = 'Size'/></li></Link>
+            <Link><li href = ''><img src = {Menu_icon} alt = '' className = 'Size'/></li></Link>
             </div> 
         </div>
      )
