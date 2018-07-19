5ac5bf597ba9681b64e01741a2629032e88c24e0
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-07-19 09:16:09 -0600

    map has seachBox with autocomplete

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, } from "react-google-maps";
+const { StandaloneSearchBox } = require("react-google-maps/lib/components/places/StandaloneSearchBox");
 
 
 class Map extends Component {
     constructor() {
         super();
         this.state = {
-            map: null
+            map: null,
+            searchBox: null,
+            center: {
+                lat: 33.64,
+                lng: -117.91
+            }
         }
     }
 
@@ -24,35 +30,80 @@ class Map extends Component {
         })
     }
 
-    zoomChanged(){
+    zoomChanged() {
         console.log('zoomChanged:' + this.state.map.getZoom())
     }
+
+    onSearchBoxMounted(searchBox){
+        if(this.state.searchBox !== null){
+            return;
+        }
+        this.setState({
+            searchBox: searchBox
+        })
+    }
+
+    onPlacesChanged(){
+        console.log('OnPlacesChanged:' + this.state.searchBox.getPlaces());
+        console.log(this.state.searchBox.getPlaces());
+        const places = this.state.searchBox.getPlaces();
+        console.log(places[0].geometry.location.lat(), places[0].geometry.location.lng);
+
+        this.setState({
+            center: {
+            lat:places[0].geometry.location.lat(),
+            lng:places[0].geometry.location.lng()
+        }
+    })
+}
 
 
     render() {
         const markers = this.props.markers || [];
 
         return (
+            <div>
 
-            <GoogleMap
-                onZoomChanged = {this.zoomChanged.bind(this)}
-                ref={this.mapLoaded.bind(this)}
-                onDragEnd={this.mapMoved.bind(this)}
-                defaultZoom={this.props.zoom}
-                defaultCenter={this.props.center}
-            >
+                <StandaloneSearchBox
+                    ref={this.onSearchBoxMounted.bind(this)}
+                    bounds={this.props.bounds}
+                    onPlacesChanged={this.onPlacesChanged.bind(this)}
+                >
+                    <input
+                        type="text"
+                        placeholder="Customized your placeholder"
+                        style={{
+                            boxSizing: `border-box`,
+                            border: `1px solid transparent`,
+                            width: `240px`,
+                            height: `32px`,
+                            padding: `0 12px`,
+                            borderRadius: `3px`,
+                            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                            fontSize: `14px`,
+                            outline: `none`,
+                            textOverflow: `ellipses`,
+                        }}
+                    />
+                </StandaloneSearchBox>
 
-                {markers.map((marker, index) => (
+                <GoogleMap
+                    onZoomChanged={this.zoomChanged.bind(this)}
+                    ref={this.mapLoaded.bind(this)}
+                    onDragEnd={this.mapMoved.bind(this)}
+                    defaultZoom={this.props.zoom}
+                    center={this.state.center}
+                >
 
-                    <Marker {...marker} />
+                    {markers.map((marker, index) => (
 
-                )
-                )}
+                        <Marker {...marker} />
 
-            </GoogleMap>
+                    )
+                    )}
+                </GoogleMap>
 
-
-
+            </div>
         );
     }
 };
